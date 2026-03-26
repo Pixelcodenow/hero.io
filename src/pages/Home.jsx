c31b321c09d70ac027af189c968ff1c12322d139
pixelcodenow <pixelcodenow@gmail.com>
2026-03-26 20:29:58 +0600

    add home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+
+import Banner from "../components/Common/Home/Banner.jsx";
+import StateCards from "../components/Common/Home/StateCards.jsx";
+import TopApps from "../components/Common/Home/TopApps.jsx";
+const Home = () => {
+  return (
+    <main>
+      <Banner />
+      <StateCards />
+      <TopApps />
+    </main>
+  );
+};
+
+export default Home;
+
+
